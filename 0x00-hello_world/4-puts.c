f2756a9f21cedd4f12eb6b511235ab7c385d86d0
BeautifulAngel246 <favouronuche@gmail.com>
2023-09-07 05:48:22 -0700

    alx processor

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (showcase)
+ */
+int main(void)
+{
+	puts("programing is like  building a multilingual puzzle\n");
+	return(0);
+}
