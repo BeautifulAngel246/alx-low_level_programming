25f09692313121c41afe94f6af20d8d2005a1cad
BeautifulAngel246 <favouronuche@gmail.com>
2023-09-21 07:07:44 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,7 +13,6 @@ void print_buffer(char *b, int size)
 	int o, j, i;
 
 	o = 0;
-
 	if (size <= 0)
 
 	{
@@ -38,6 +37,7 @@ void print_buffer(char *b, int size)
 		for (i = 0; i < j; i++)
 		{
 			int c = *(b + o + i);
+
 			if (c < 32 || c > 132)
 			{
 				c = '.';
