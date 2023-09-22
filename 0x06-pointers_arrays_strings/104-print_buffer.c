8e573203fb67486c2dad12c0016edfd426989723
BeautifulAngel246 <favouronuche@gmail.com>
2023-09-22 11:32:10 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,27 +10,43 @@
 
 void print_buffer(char *b, int size)
 {
-	int i, j;
-	char c;
+	int x;
+	int y;
+	int z;
 
-	if (size < 1)
-		printf("\n");
+	x = 0;
+
+	if (size <= 0)
+	{
+	printf("\n");
+	return;
+	}
+	while (x < size)
+	{
+	y = size - x < 10 ? size -  x : 10;
+	printf("%08x: ", x);
+	for (z = 0; z < 10; z++)
+	{
+	if (z < y)
+	printf("%02x", *(b + x + z));
 	else
-		for (i = 0; i < size; i += 10)
-		{
-			printf("%08x: ", i);
-			for (j = i; j < i + 10 && j < size; j += 2)
-				printf("%02x%02x ", (unsigned char)b[j], (unsigned char)b[j + 1]);
-			for (; j < i + 10; j += 2)
-				printf("     ");
-			for (j = i; j < i + 10 && j < size; j++)
-			{
-				c = b[j];
-				if (c >= ' ' && c <= '~')
-					printf("%c", c);
-				else
-					printf(".");
-			}
-			printf("\n");
-		}
+	printf(" ");
+	if (z % 2)
+	{
+	printf(" ");
+	}
+	}
+	for (z = 0; z < y; z++)
+	{
+	int c = *(b + x + z);
+
+	if (c < 32 || c > 132)
+	{
+	c = '.';
+	}
+	printf("%c", c);
+	}
+	printf("\n");
+	x += 10;
+	}
 }
