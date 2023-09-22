56d8a30cae630119e9e7897d7229962f215d3ed1
BeautifulAngel246 <favouronuche@gmail.com>
2023-09-22 15:15:25 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,52 +1,58 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * print_buffer - prints a buffer
- * @b: buffer
- * @size: size of buffer
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
  * Return: void
  */
 
+void print_line(char *c, int s, int l)
+{
+int j, k;
+for (j = 0; j <= 9; j++)
+{
+if (j <= s)
+printf("%02x", c[l * 10 + j]);
+else
+printf("  ");
+if (j % 2)
+putchar(' ');
+}
+for (k = 0; k <= s; k++)
+{
+if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+putchar(c[l * 10 + k]);
+else
+putchar('.');
+}
+}
+
+/**
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
 void print_buffer(char *b, int size)
 {
-	int x;
-	int y;
-	int z;
-
-	x = 0;
-
-	if (size <= 0)
-	{
-	printf("\n");
-	return;
-	}
-	while (x < size)
-	{
-	y = size - x < 10 ? size -  x : 10;
-	printf("%08x: ", x);
-	for (z = 0; z < 10; z++)
-	{
-	if (z < y)
-	printf("%02x", *(b + x + z));
-	else
-	printf(" ");
-	if (z % 2)
-	{
-	printf(" ");
-	}
-	}
-	for (z = 0; z < y; z++)
-	{
-	int c = *(b + x + z);
-
-	if (c < 32 || c > 132)
-	{
-	c = '.';
-	}
-	printf("%c", c);
-	}
-	printf("\n");
-	x += 10;
-	}
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
+{
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
+}
+else
+{
+print_line(b, size % 10 - 1, i);
+}
+putchar('\n');
+}
+if (size == 0)
+putchar('\n');
 }
