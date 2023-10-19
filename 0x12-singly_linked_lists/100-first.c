9f74c36b201da02f49c8b36e09f039d2164d4713
BeautifulAngel246 <favouronuche@gmail.com>
2023-10-19 15:45:47 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ * before_main - printing a message before main
+ * Return: void
+ */
+
+void before_main(void)
+{
+	P("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
