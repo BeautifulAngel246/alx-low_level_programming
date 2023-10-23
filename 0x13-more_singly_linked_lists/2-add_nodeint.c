c685696fa003d9165168fed6f9952aad65b9629c
BeautifulAngel246 <favouronuche@gmail.com>
2023-10-23 14:46:57 -0700

    two nodding

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - adding a new node at the beginning of a listint_t list.
+ * @head: pointer to the first node in the list
+ * @n: data to insert in new node
+ * Return: pointer to new node, or NULL if it fails
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = (listint_t *)malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
