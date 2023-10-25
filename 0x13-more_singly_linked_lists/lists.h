d40f8ea4225d0fbfeada5f6e39b0b24d6a328e7f
BeautifulAngel246 <favouronuche@gmail.com>
2023-10-25 14:51:40 -0700

    beautifully linked

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -32,6 +32,7 @@ int delete_nodeint_at_index(listint_t **, unsigned int);
 listint_t *reverse_listint(listint_t **);
 size_t print_listint_safe(const listint_t *);
 size_t check_looped_listint(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *);
 
 #endif
