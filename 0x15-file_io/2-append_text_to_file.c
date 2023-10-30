6d2f3229e3fab762b09716b6649613d894bbe8c1
BeautifulAngel246 <favouronuche@gmail.com>
2023-10-30 07:47:54 -0700

    appending files

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: filename.
+ * @text_content: added content.
+ * Return: 1 if the file exists. -1 if the fails does not exist
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len = 0;
+	ssize_t bytes;
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+		bytes = write(fd, text_content, len);
+		if (bytes == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
+}
